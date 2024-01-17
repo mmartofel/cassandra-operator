4555f9c99130e89a18a59d7bf5e2bf451b23ddc2
mmartofel <mmartofe@redhat.com>
2024-01-17 12:56:13 +0100

    moove cluster to mycassandra namespace

--- connect.sh
+++ connect.sh
@@ -1,6 +1,6 @@
 
-USERNAME=`oc get secret cluster1-superuser -o jsonpath="{.data.username}" | base64 --decode`
-PASSWORD=`oc get secret cluster1-superuser -o jsonpath="{.data.password}" | base64 --decode ; echo`
+USERNAME=`oc get secret cluster1-superuser -o jsonpath="{.data.username}" -n mycassandra | base64 --decode`
+PASSWORD=`oc get secret cluster1-superuser -o jsonpath="{.data.password}" -n mycassandra | base64 --decode ; echo`
 
 echo cqlsh -u $USERNAME -p $PASSWORD
 
--- setup.sh
+++ setup.sh
@@ -1,19 +1,20 @@
 # Delete beforehand
-oc delete CassandraDatacenter cassandra-dc1
+oc new-project mycassandra
+oc delete CassandraDatacenter cassandra-dc1 -n mycassandra
 sleep 10
-oc delete sa cass-sa
-oc delete secret cluster1-superuser
+oc delete sa cass-sa -n mycassandra
+oc delete secret cluster1-superuser -n mycassandra
 
 
 # Create service account for Cassandra server 
-oc create sa cass-sa
+oc create sa cass-sa -n mycassandra
 
 # Grant anuuid (server runs under 999 uid which is forbiden)
-oc adm policy add-scc-to-user anyuid -z cass-sa
+oc adm policy add-scc-to-user anyuid -z cass-sa -n mycassandra
 
 # Create superuser secret to fix creds to cluster1-superuser/cassandra for your credentials
-# oc apply -f cluster1-superuser-secret.yaml
+# oc apply -f cluster1-superuser-secret.yaml -n mycassandra
 
 # Create Cassandra cluster according to your yaml
-oc apply -f cassandradatacenter-cassandra-dc1.yaml
+oc apply -f cassandradatacenter-cassandra-dc1.yaml -n mycassandra
 
